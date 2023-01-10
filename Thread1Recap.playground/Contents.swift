c4369e1a75de64bf43416c48dd2252e0de7ae881
Tom <twu@lcs.on.ca>
2023-01-10 11:22:27 -0500

    Q1-6

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -3,3 +3,20 @@ import Cocoa
 var greeting = "Hello, playground"
 //Q1: int
 //Q2: float
+
+struct Dog {
+    let name: String
+    let breed: String
+    let color: String
+    var heightInMetres: Double
+    
+    var heightInCentimetres: Double{
+        return heightInMetres*100
+    }
+    var length: Float
+    var mass: Float
+    
+    var ratio: Float{
+        return length*mass
+    }
+}
